b3fcd6d37076c3a36f337cbf72c023d29432bd52
AndreyNicollas <adsandr3y24@gmail.com>
2026-03-07 16:26:51 -0300

    fix(RES-14): atualizado o ResenhaMapper e corrigido o application-dev.yaml com variaveis de ambiente

--- src/main/java/com/resenhita/mapper/ResenhaMapper.java
+++ src/main/java/com/resenhita/mapper/ResenhaMapper.java
@@ -3,6 +3,8 @@ package com.resenhita.mapper;
 import com.resenhita.dto.ResenhaDto;
 import com.resenhita.model.entity.Resenha;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ResenhaMapper {
@@ -10,4 +12,7 @@ public interface ResenhaMapper {
     Resenha deDtoParaEntidade(ResenhaDto resenhaDto);
 
     ResenhaDto deEntidadeParaDto(Resenha resenha);
+
+    @Mapping(target = "id", ignore = true)
+    void atualizarEntidadeDoDto(ResenhaDto resenhaDto, @MappingTarget Resenha resenha);
 }
